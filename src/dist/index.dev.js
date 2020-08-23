8ffcf2796d98e927b857b8afaa91d584cc6c0b19
Thompson Maxdwell <oghenefokethompson@gmail.com>
2020-08-23 21:52:32 +0000

    Done with footer and navbar

---
+++ src/dist/index.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _navbar = _interopRequireDefault(require("./header/navbar"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _navbar["default"])();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import {nav} from "./modules/nav";
-
-console.log(nav('john', 22,'football'));
-
-console.log(
-'YEAH working... is it not really'
-);
+import navFun  from './header/navbar';
+navFun()
