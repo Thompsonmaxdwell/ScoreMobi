d622250c1aeca1f7bfec7492e07dee009c9ddb2d
john ifemezuo <johnifemezuoanayo@gmail.com>
2020-08-24 21:50:38 +0100

    n

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@ import {nav} from "./modules/nav";
 console.log(nav('john', 22,'football'));
 
 console.log(
-'YEAH working... is it not really'
+'YEAH working... is it not really.....ani nnwanma'
 );
+
+// Write a loop that makes seven calls to console.log to output the following
+// triangle:
+
+let text = ['#','#','#','#','#','#','#'];
+let shot = ""
+
+for (let i = 0; i < 7; i++) {
+     shot += text[i]
+console.log(shot);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
