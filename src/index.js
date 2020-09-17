939adfdf305136a8840284fb4cfda3694627e5e0
Thompson Maxdwell <oghenefokethompson@gmail.com>
2020-09-17 03:22:01 +0000

    Done with the homepage second slide

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const nextButton = document.querySelector('.carousel_button-right');
-const prevButton = document.querySelector('.carousel_button-left');
+const nextButtons = document.querySelectorAll('.carousel_button-right');
+const prevButtons = document.querySelectorAll('.carousel_button-left');
 
 import {moveSlideToRight, moveSlideToLeft } from './slide/slide';
 import navFun  from './header/navbar';
 import tab from './tabs/tabs';
 
 
-
 /*****************************************************************************/
-nextButton.addEventListener('click', moveSlideToRight);
-prevButton.addEventListener('click', moveSlideToLeft);
-setInterval(moveSlideToRight, 5000);
+
+nextButtons.forEach(nextButton => nextButton.addEventListener('click', moveSlideToRight))
+prevButtons.forEach(prevButton => prevButton.addEventListener('click', moveSlideToLeft))
+// setInterval(moveSlideToRight, 5000);
 /*****************************************************************************/
 
 // tabs of the league page section
